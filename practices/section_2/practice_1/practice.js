cf089bc599e20d2267c5cdf4d04941f609994da9
dhxyyz <hd964660612@outlook.com>
2015-05-17 23:00:48 +0800

    2-1

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,14 +1,20 @@
 function count_same_elements(collection) {
   //在这里写入代码
-  var listMap={};
-  for(i=0;i<collection.length;i++){
-    if (!!listMap[collection[i]]) {
-            listMap[collection[i]]++;
-        } else {
-            listMap[key] = 1;
-        }
-        return listMap;
-  }
-}
 
+  var target={};
+   var array=[];
+   var test=true;
+   for (i=0;i<collection.length;i++){
+     if(test){
+       target={key:collection[i],count:0};
+       test=false;
+     }
+     target.count=target.count+1;
+     if(collection[i]!=collection[i+1]){
+       array.push(target);
+       test=true;
+     }
+   }
+   return array;
+ }
 module.exports = count_same_elements;
--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -4,7 +4,8 @@ function create_updated_collection(collection_a, object_b) {
   for (i=0;i<collection_a.length;i++){
     for(x=0;x<object_b.value.length;x++){
       if (collection_a[i].key==object_b.value[x]){
-        collection_a[i].count=collection_a[i].count-(collection_a[i].count-collection_a[i].count%3)/3;
+    //    collection_a[i].count=collection_a[i].count-(collection_a[i].count-collection_a[i].count%3)/3;
+        collection_a[i].count=collection_a[i].count-parseInt(collection_a[i].count/3);
       }
     }
   }
