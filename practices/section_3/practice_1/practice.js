a0ae9eabd7d1c8d55e59f81396bd037e6f7f4110
dhxyyz <hd964660612@outlook.com>
2015-05-17 16:43:28 +0800

    又一次

--- practices/section_3/practice_1/practice.js
+++ practices/section_3/practice_1/practice.js
@@ -1,8 +1,8 @@
 function create_updated_collection(collection_a, object_b) {
 
   for (i=0;i<collection_a.length;i++){
-    for(j=0;j<object_b.value.length;j++){
-      if (collection_a[i].key==object_b.value[j]){
+    for(x=0;x<object_b.value.length;x++){
+      if (collection_a[i].key==object_b.value[x]){
         collection_a[i].count=collection_a[i].count-1;
       }
     }
--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -2,8 +2,8 @@ function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
 
   for (i=0;i<collection_a.length;i++){
-    for(j=0;j<object_b.value.length;j++){
-      if (collection_a[i].key==object_b.value[j]){
+    for(x=0;x<object_b.value.length;x++){
+      if (collection_a[i].key==object_b.value[x]){
         collection_a[i].count=collection_a[i].count-(collection_a[i].count-collection_a[i].count%3)/3;
       }
     }
