52f0edfd6e1d45973dfdadf8d4f6465f1d53c3e3
dhxyyz <hd964660612@outlook.com>
2015-05-17 23:41:01 +0800

    success

--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -2,7 +2,7 @@ function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
   var target={};
    var array=[];
-   var array_a=[];
+
    var test=true;
    for (i=0;i<collection_a.length;i++){
      if(test){
@@ -20,8 +20,7 @@ function create_updated_collection(collection_a, object_b) {
    for (i=0;i<array.length;i++){
      for(x=0;x<object_b.value.length;x++){
        if (array[i].key==object_b.value[x]){
-     //    collection_a[i].count=collection_a[i].count-(collection_a[i].count-collection_a[i].count%3)/3;
-     array[i].count=array[i].count-parseInt(array[i].count/3);
+         array[i].count=array[i].count-parseInt(array[i].count/3);
        }
      }
    }
--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,33 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
-}
+  var target={};
+  var array=[];
+  var test=true;
+  for (i=0;i<collection_a.length;i++){
+    if(test){
+      target={key:collection_a[i].charAt(0),count:0};
 
+      test=false;
+    }
+    target.count=target.count+1;
+    for(y=0;y<10;y++){
+      if(collection_a[i].indexOf(y)>-1){
+        target.count=target.count+y-1;
+      }
+    }
+    if(collection_a[i]!=collection_a[i+1]){
+      array.push(target);
+      test=true;
+    }
+  }
+
+  for (i=0;i<array.length;i++){
+    for(x=0;x<object_b.value.length;x++){
+      if (array[i].key==object_b.value[x]){
+        array[i].count=array[i].count-parseInt(array[i].count/3);
+      }
+    }
+  }
+  return array;
+}
 module.exports = create_updated_collection;
