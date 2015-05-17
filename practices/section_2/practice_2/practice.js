e011133a7c9a73ad0ced78e898855f9168d22c68
dhxyyz <hd964660612@outlook.com>
2015-05-17 23:29:17 +0800

    3_3

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,28 @@
 function count_same_elements(collection) {
   //在这里写入代码
-}
+
+     var target={};
+     var array=[];
+     var test=true;
+     for (i=0;i<collection.length;i++){
+       if(test){
+         target={key:collection[i].charAt(0),count:0};
+
+         test=false;
+       }
+       target.count=target.count+1;
+       for(y=0;y<10;y++){
+         if(collection[i].indexOf(y)>-1){
+           target.count=target.count+y-1;
+         }
+       }
+       if(collection[i]!=collection[i+1]){
+         array.push(target);
+         test=true;
+       }
+     }
+     return array;
+   }
+
 
 module.exports = count_same_elements;
